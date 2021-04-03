cbc5d5b20f313c266f8fec901a7f2bf55942869b
Rhad <79535319+arecee3@users.noreply.github.com>
2021-04-03 06:18:12 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-var startEL = document.querySelector('#start-game');
-var questionList = document.querySelector('#questionsplace');
-var startButton = document.getElementById('start-button');
+var startEL = document.querySelector("#start-game");
+var questionList = document.querySelector("#theQuestions");
+var startButton = document.getElementById("start-button");
 var timer = document.getElementById("timer");
 var timeLeft = 60;
  var timerInterval;
  var hasgameStarted = false;
- var questionElement = document.querySelector('#questionsplace');
- var answersElement = document.querySelector('#answer-buttons');
+ var questionElement = document.querySelector("#theQuestions");
+ var answersElement = document.querySelector("#answer-buttons");
  var gameIndex = 0;
  var questions = [
 { 
@@ -32,37 +32,37 @@ answer: "Executive"
 ];
 
 var hasGameEnded= false;
-var endGameElement = document.querySelector('#end-screen');
-var playerName = ''
+var endGameElement = document.querySelector("#end-screen");
+var initials = "";
 var score = 0;
-var playerScoreList = JSON.parse(localStorage.getItem('yourScore')) || [];
+var playerScoreList = JSON.parse(localStorage.getItem("yourScore")) || [];
 
-startButton.addEventListener('click', function() {
+startButton.addEventListener("click", function() {
 startTime();
 startGame();
 startQuestions();
 })
 
 function startTime() {
-    timerInterval = setInterval(function(){
+    timerInterval = setInterval(function() {
         if (timeLeft <= 1){
             clearInterval(timerInterval);
-            endgame()
+            endgame();
         }
         timeLeft--;
-        timer.textContent = 'Time Left: ' + timeLeft;
+        timer.textContent = "Time Left: " + timeLeft;
     },  1000)
 }
-function startGame(){
+function startGame() {
     hasgameStarted = true;
-    startEL.setAttribute('class', 'hidden');
-    questionElement.setAttribute('class', 'visible');
+    startEL.setAttribute("class", "hidden");
+    questionElement.setAttribute("class", "visible");
 }
 function startQuestions () {
     questionElement.textContent = questions[gameIndex].title;
-    answersElement.innerHTML = '';
+    answersElement.innerHTML = " ";
  for ( var i = 0; i < questions[gameIndex].options.length; i++) {
-        var btn = document.createElement('button');
+        var btn = document.createElement("button");
         btn.textContent = questions[gameIndex].options[i];
         btn.onclick = checkAnswer;
         answersElement.append(btn)
@@ -70,10 +70,10 @@ function startQuestions () {
 }
 function checkAnswer() {
     if (this.textContent === questions[gameIndex].answer){
-        alert('Correct');
+        alert("Correct");
         score++;
     } else {
-        alert('Incorrect');
+        alert("Incorrect");
         timeLeft -= 10;
     }
     gameIndex++;
@@ -85,22 +85,22 @@ function checkAnswer() {
         startQuestions();
     }
 }
-document.querySelector('#submittedName').addEventListener('click', function() {
-firtsName = document.querySelector('#playerName').value;
+document.querySelector("#submittedName").addEventListener("click", function() {
+firtsName = document.querySelector("#initials").value;
 endGameElement();
 })
 function playerName() {
-    document.getElementById('yourScore').textContent = 'Your score is '
+    document.getElementById("youScore").textContent = "Your Score is  "
 + score;
-document.querySelector('#end-screen').classList.remove('hidden');
-questionElement.setAttribute('class', 'hidden')
+document.querySelector("#end-screen").classList.remove("hidden");
+questionElement.setAttribute("class", "hidden")
 }
 
 function endgame() {
     hasGameEnded = true;
-    endGameElement.setAttribute('class' , 'visisble');
-    playerScoreList.push({'playerName': playerName, 'score': score});
-    localStorage.setItem('highscore' , JSON.stringify(playerScoreList));
+    endGameElement.setAttribute("class" , "visible");
+    playerScoreList.push({"initials": playerName, "score" : score});
+    localStorage.setItem("highscore" , JSON.stringify(playerScoreList));
 
     window.location.assign('./score.html');
 }
